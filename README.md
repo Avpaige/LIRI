2354396eb7adb52a706bde97f63d573b46615230
Ari Virgil-Paige <avpaige@gmail.com>
2019-08-15 23:24:45 -0700

    updates to spotify and README

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
 # LIRI
+
+This LIRI bot allows you to quickly search a variety of entertainment databases to find something to quickly listen to (Spotify API), Movie to watch (oMDB API) or upcoming concerts to attend (Bands in Town API)
+
+Bored and looking for something to do but don't where to search, or worse don't want to search in multiple places to find options. This LIRI bot is here to help.
+
+Structured to function as a single terminal file to search via node you can search song titles, upcoming concerts or movies by using simple commands.
+
+
+To run this program on your terminal follow the below steps:
+
+    1. Create your own Spotify access key via the API. By going to:
+        Do this by:
+            Step One: Visit https://developer.spotify.com/my-applications/#!/
+            Step Two: Either login to your existing Spotify account or create a new one (a free account is fine) and log in.
+            Step Three: Once logged in, navigate to https://developer.spotify.com/my-applications/#!/applications/create to register a new application to be used with the Spotify API. You can fill in whatever you'd like for these fields. When finished, click the "complete" button.
+            Step Four: On the next screen, scroll down to where you see your client id and client secret. Copy these values down somewhere, you'll need them to use the Spotify API and the node-spotify-api package.
+        Once you have your keys:
+            Set up your own .env file within the root of the project folder updated with your own API kets
+            # Spotify API keys
+
+            SPOTIFY_ID=your_APP_ID_here
+            SPOTIFY_SECRET=your_API_secret_here
+    2. Run npm install to install all dependcies associated with this app.
+    3. To search use the following commands:
+        concert-this YOUR ARTIST/BAND SEARCH
+        spotify-this-song YOUR SONG TITLE SEARCH
+        movie-this YOUR MOVIE SEARCH
+        do-what-it-says SURPRISE
+
+
+This LIRI bot utilizies: JQuery, Moment, Noda and APIs from Spotify, Bands in Town and oMDB 
+Link: https://avpaige.github.io/LIRI/ 
+I developed this application as part of the UCLA Web Developement Extension course work.
+
+
+
--- liri.js
+++ liri.js
@@ -1,15 +1,35 @@
-// require("dotenv").config();
-// var fs = require("fs");
-// var inquirer = require("inquirer");
-// var spotify = new Spotify(keys.spotify);
-// var keys = require("./keys.js");
-
+require("dotenv").config();
+var keys = require("./keys.js");
+var fs = require("fs");
+var Spotify = require("node-spotify-api");
+var spotify = new Spotify(keys.spotify);
 var axios = require("axios");
 var moment = require('moment');
 var search = process.argv[3];
 var input = process.argv[2];
 
+// var inquirer = require("inquirer");
+
 if (input==="movie-this"){
+    if(search === undefined){
+    axios.get("http://www.omdbapi.com/?t=Mr.Nobody&y=&plot=short&apikey=trilogy")
+.then(
+  function(response) {
+    console.log("You didn't enter anything! If you haven't watched Mr. Nobody, then you should! It's on Netflix!")
+    console.log("Title: " + response.data.Title);
+    console.log("Year Released: " + response.data.Year);
+    console.log("iMDB Rating: " + response.data.imdbRating);
+    console.log("Rotten Tomatoes Rating: " + response.data.Ratings[1].Value);
+    console.log("Produced in: " + response.data.Country);
+    console.log("Language: " + response.data.Language);
+    console.log("Plot: " + response.data.Plot);
+    console.log("Actors: " + response.data.Actors);
+  })
+  .catch(function(error) {
+    if (error.response) {    
+    }
+  });
+}else{
 axios.get("http://www.omdbapi.com/?t=" +search+ "&y=&plot=short&apikey=trilogy")
 .then(
   function(response) {
@@ -26,7 +46,9 @@ axios.get("http://www.omdbapi.com/?t=" +search+ "&y=&plot=short&apikey=trilogy")
     if (error.response) {    
     }
   });
-}if (input==="concert-this"){
+ }
+}
+if (input==="concert-this"){
     axios.get("https://rest.bandsintown.com/artists/"+search+"/events?app_id=codingbootcamp")
     .then(function(response) {   
     var results = response.data
@@ -41,11 +63,26 @@ axios.get("http://www.omdbapi.com/?t=" +search+ "&y=&plot=short&apikey=trilogy")
     console.log("Event Date: "+ eventDate);
     console.log("-----------------------------")
         }   
-    }
-
+    } 
 })
     .catch(function(error) {
     if (error.response) {    
         }
     });
 }
+
+if (input==="spotify-this-song"){
+    spotify.search({type:"track",query: search,},
+    function (error, data){
+    if (error) {  
+        console.log("Hmmm we don't know that one, can you hum it?")
+    }else{
+      var songs= repsonse.data  
+       for (song of songs){
+            console.log(song)
+                    }
+                }
+            }
+        );
+    }
+    
